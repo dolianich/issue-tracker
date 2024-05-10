0ae10439f40928cd1638c131f5f7223ae51b7fe0
dolianich <69437697+dolianich@users.noreply.github.com>
2024-05-10 22:32:36 +0300

    Remove duplicate skeletons

--- app/issues/edit/[id]/page.tsx
+++ app/issues/edit/[id]/page.tsx
@@ -4,9 +4,9 @@ import { notFound } from 'next/navigation';
 import dynamic from 'next/dynamic';
 import IssueFormSkeleton from '../../_components/IssueFormSkeleton';
 
-const IssueForm = dynamic(() => import('@/app/issues/_components/IssueForm'),
-{ssr: false,
-    loading: () => <IssueFormSkeleton/>
+const IssueForm = dynamic(() => import('@/app/issues/_components/IssueForm'), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
 });
 interface Props {
   params: { id: string };
