6e6b683c74ba1f571256f283285e92548849f3e9
dolianich <69437697+dolianich@users.noreply.github.com>
2024-05-11 14:55:28 +0300

    Secure the application

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,11 +1,16 @@
+import authOptions from "@/app/auth/authOptions";
 import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
 import delay from "delay";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 
 
 export async function PATCH(request: NextRequest, {params}: { params: {id: string} }){
+    const session = await getServerSession(authOptions);
+    if (!session)
+        return NextResponse.json({}, {status: 401});
     const body = await request.json();
     const validation = issueSchema.safeParse(body);
     if(!validation.success)
@@ -30,6 +35,9 @@ export async function PATCH(request: NextRequest, {params}: { params: {id: strin
 
 export async function DELETE(request: NextRequest, {params}: { params: {id: string} }){
     await delay(2000);
+    const session = await getServerSession(authOptions);
+    if (!session)
+        return NextResponse.json({}, {status: 401});
     const issue = await prisma.issue.findUnique({
             where: {id: parseInt(params.id)}
         });
