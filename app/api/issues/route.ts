1670f63c55eb465a79aa14725c08f59d102e2c8f
dolianich <69437697+dolianich@users.noreply.github.com>
2024-05-14 11:09:54 +0300

    Enhance the API to assign issues

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,6 +1,7 @@
 import authOptions from "@/app/auth/authOptions";
-import { issueSchema } from '@/app/validationSchemas';
+import { patchIssueSchema } from '@/app/validationSchemas';
 import prisma from "@/prisma/client";
+import { error } from "console";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -10,12 +11,19 @@ export async function POST(request: NextRequest) {
     if (!session)
         return NextResponse.json({}, {status: 401});
     const body = await request.json();
-    const validation = issueSchema.safeParse(body);
+    const validation = patchIssueSchema.safeParse(body);
     if (!validation.success)
         return NextResponse.json(validation.error.errors, {status: 400});
 
+    const {assignedToUserId, title, description} = body;
+    if(assignedToUserId) {
+        const user = await prisma.user.findUnique({where: {id: assignedToUserId}})
+        if(!user)
+            return NextResponse.json({error: 'Invalid User.'}, {status: 400})
+    }
+
     const newIssue = await prisma.issue.create({
-        data: {title: body.title, description: body.description}
+        data: {title, description, assignedToUserId}
     });
 
     return NextResponse.json(newIssue, {status: 201});
